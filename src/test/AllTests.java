1e65fe75871e607e586af2d3de5819357aa2efb2
Dijana <deny.dijana@gmail.com>
2016-04-12 18:25:49 +0200

    One comment added

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -3,7 +3,7 @@ package test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
+// THIS IS A CLASS NAMED SUITE FROM JUNIT CLASSES WITCH CONTAIN ALL MY TESTS CLASSES
 @RunWith(Suite.class)
 @SuiteClasses({ countATest.class, squareTest.class })
 public class AllTests {
